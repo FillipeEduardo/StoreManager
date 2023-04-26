cc1b80475cdb64722c484cac0131e51ae8297911
FillipeEduardo <fillipe_oliveira@outlook.com.br>
2023-04-26 18:38:03 -0300

    criacao do primeiro teste para o endpoint getAllProducts

---
+++ StoreManagerTest/Mocks/ProductMock.cs
@@ -0,0 +1,15 @@
+﻿using StoreManager.Models;
+
+namespace StoreManagerTest.Mocks;
+
+public static class ProductMock
+{
+    public static List<Product> ReturnListProducts()
+    {
+        var products = new List<Product>();
+        products.Add(new Product { Id = 1, Name = "Martelo de Thor" });
+        products.Add(new Product { Id = 2, Name = "Traje de encolhimento" });
+        products.Add(new Product { Id = 3, Name = "'Escudo do Capitão América'" });
+        return products;
+    }
+}
---
+++ StoreManagerTest/ProductTests/ProductTests.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using StoreManager.Abstractions.Repositories;
+using StoreManager.Controllers;
+using StoreManager.DTOs;
+using StoreManager.Services;
+using StoreManagerTest.Mocks;
+
+namespace StoreManagerTest.ProductTests;
+
+public class ProductTests
+{
+    private readonly Mock<IProductRepository> _productRepository;
+    private readonly IMapper _mapper;
+
+    public ProductTests()
+    {
+        _productRepository = new Mock<IProductRepository>();
+        _mapper = new MapperConfiguration(mm =>
+        {
+            mm.AddProfile(new StoreProfile());
+        }).CreateMapper();
+    }
+
+    [Fact]
+    public async Task GetAllProducts_Executed_Success()
+    {
+        var productsMock = ProductMock.ReturnListProducts();
+        _productRepository.Setup(pr => pr.GetAll()).Returns(Task.FromResult(productsMock));
+
+        var productService = new ProductService(_productRepository.Object, _mapper);
+        var productController = new ProductController(productService);
+
+        var productResult = (OkObjectResult)await productController.Get();
+        Assert.Equivalent(productResult.Value, productsMock);
+    }
+
+}
